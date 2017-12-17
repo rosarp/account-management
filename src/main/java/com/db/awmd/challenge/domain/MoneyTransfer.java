1630531b57374d700179bf3ed0af1dc305e6e490
rosarp <rohi7ns@gmail.com>
2017-12-17 19:03:30 +0530

    Validation added to amount in MoneyTransfer.

--- src/main/java/com/db/awmd/challenge/domain/MoneyTransfer.java
+++ src/main/java/com/db/awmd/challenge/domain/MoneyTransfer.java
@@ -2,6 +2,7 @@ package com.db.awmd.challenge.domain;
 
 import java.math.BigDecimal;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotEmpty;
@@ -10,6 +11,13 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
+
+/**
+ * Money Transfer object used to transfer money from one account to other.
+ * 
+ * @author rosarp
+ *
+ */
 
 @Data
 public class MoneyTransfer {
@@ -22,6 +30,7 @@ public class MoneyTransfer {
   private final String accountTo;
 
   @NotNull
+  @Min(value = 0, message = "Initial balance must be positive.")
   private BigDecimal amount;
 
   @JsonCreator
